0c9ee49f8e1747b734bd64b3b2110597fc126a67
transfact <transpaction@naver.com>
2023-12-18 23:39:54 +0900

    local

--- src/Page/PostPage.js
+++ src/Page/PostPage.js
@@ -1,8 +1,6 @@
 import useLoginCheck from '../CustomHook/useLoginCheck';
 
 export default function PostPage() {
-    const loginCheck = useLoginCheck();
-
     return (
         <div>
             <h1>Post Page</h1>
